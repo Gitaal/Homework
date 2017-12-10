97b0a5258dddc5325d7c49efb1dca35a11457d12
Alya <evillisi4@yandex.ru>
2017-12-11 00:11:06 +0400

    Добавлена программа для вывода информации по подарку

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -22,7 +22,7 @@ public class Application {
         System.out.println("Введите 1 - калькулятор");
         System.out.println("Введите 2 - поиск наимбольшего слова в массиве");
         System.out.println("Введите 3 - замена наименьшего отрицательного числа с наименьшим положительным");
-        System.out.println("Введите 4 - для информации по поадрку");
+        System.out.println("Введите 4 - получить информацию по подарку");
         int vibor = scanner.nextInt();
         switch (vibor) {
             case 1:
